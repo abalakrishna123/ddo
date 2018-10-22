dcd23929ecf7f3c895947cc3b944b3852eb7304d
Ashwin Balakrishna <abalakrishna@gmail.com>
2018-10-22 11:04:24 -0700

    Added simple experimentation script

---
+++ run_ddo_experiment.py
@@ -0,0 +1,85 @@
+import numpy as np
+from segmentcentroid.envs.GridWorldEnv import GridWorldEnv
+from segmentcentroid.planner.value_iteration import ValueIterationPlanner
+from segmentcentroid.tfmodel.GridWorldModel import GridWorldModel
+import tensorflow as tf
+import copy
+
+gmap = np.loadtxt('resources/GridWorldMaps/experiment1.txt', dtype=np.uint8)
+
+m  = GridWorldModel(2, statedim=(10,20))
+
+demonstrations = 50
+
+full_traj = []
+vis_traj = []
+
+for i in range(demonstrations):
+    print("Traj",i)
+    g = GridWorldEnv(copy.copy(gmap), noise=0.3)
+
+    v = ValueIterationPlanner(g)
+    traj = v.plan(max_depth=100)
+    # g.visualizePlan(traj)
+    print("Beg Traj")
+    print(traj)
+    print("End Traj")
+
+    new_traj = []
+    for t in traj:
+        a = np.zeros(shape=(4,1))
+
+        s = np.zeros(shape=(10,20))
+
+        a[t[1]] = 1
+
+        s[t[0][0],t[0][1]] = 1
+        #s[2:4,0] = np.argwhere(g.map == g.START)[0]
+        #s[4:6,0] = np.argwhere(g.map == g.GOAL)[0]
+
+        new_traj.append((s,a))
+
+    full_traj.append(new_traj)
+    vis_traj.extend(new_traj)
+
+m.sess.run(tf.initialize_all_variables())
+
+with tf.variable_scope("optimizer"):
+    opt = tf.train.GradientDescentOptimizer(learning_rate=10)
+
+    m.train(opt, full_traj, 10000, 0)
+
+actions = np.eye(4)
+
+
+g = GridWorldEnv(copy.copy(gmap), noise=0.1)
+g.generateRandomStartGoal()
+
+for i in range(m.k):
+    states = g.getAllStates()
+    policy_hash = {}
+    trans_hash = {}
+
+    for s in states:
+
+        t = np.zeros(shape=(10,20))
+
+        t[s[0],s[1]] = 1
+        #t[2:4,0] = np.argwhere(g.map == g.START)[0]
+        #t[4:6,0] = np.argwhere(g.map == g.GOAL)[0]
+
+
+        l = [ np.ravel(m.evalpi(i, [(t, actions[j,:])] ))  for j in g.possibleActions(s)]
+
+        if len(l) == 0:
+            continue
+
+        #print(i, s,l, m.evalpsi(i,ns))
+        action = g.possibleActions(s)[np.argmax(l)]
+
+        policy_hash[s] = action
+
+        #print("Transition: ",m.evalpsi(i, [(t, actions[1,:])]), t)
+        trans_hash[s] = np.ravel(m.evalpsi(i, [(t, actions[1,:])]))
+
+    g.visualizePolicy(policy_hash, trans_hash, blank=True, filename="resources/results/jeb"+str(i)+".png")
